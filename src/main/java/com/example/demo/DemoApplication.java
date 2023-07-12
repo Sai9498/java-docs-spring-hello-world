99d998829bc74bdda5d7d41ffd3644f4ba8d6c4c
Sai9498 <52398738+Sai9498@users.noreply.github.com>
2023-07-12 11:25:37 -0400

    Hello Azure

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -15,6 +15,6 @@ public class DemoApplication {
 
 	@RequestMapping("/")
 	String sayHello() {
-		return "Hello World!, This is Sai Teja - 500221271";
+		return "Hello World! This is Sai Teja";
 	}
 }
